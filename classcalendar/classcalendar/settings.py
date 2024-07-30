a2adac69c4d11c207a1871b6e35bfc4349a01dd2
Wei Wen Zhou <weiwen.zhou@stonybrook.edu>
2024-07-30 02:08:56 -0400

    add restframework

--- classcalendar/classcalendar/settings.py
+++ classcalendar/classcalendar/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'classes',
 ]
 
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
