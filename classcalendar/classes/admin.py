0795d5e1c66af63022120dcb34bc4dbe888b0e30
Wei Wen Zhou <weiwen.zhou@stonybrook.edu>
2024-07-30 02:39:43 -0400

    create models

--- classcalendar/classes/admin.py
+++ classcalendar/classes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Course, Class
 
 # Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at')
+    search_fields = ('name', 'description')
+    list_filter = ('created_at',)
+
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('course', 'start_time', 'end_time', 'created_at')
+    search_fields = ('course__name',)
+    list_filter = ('course__name', 'start_time', 'end_time', 'created_at')
--- classcalendar/classes/models.py
+++ classcalendar/classes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(default="Replace with description.")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+class Class(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.course.name}"
